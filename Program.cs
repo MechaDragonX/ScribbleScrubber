47c52339111a4cb4430ab82b8374a6151f8c0ece
MechaDragonX <theofficialrv@gmail.com>
2020-10-05 18:17:39 -0700

    Write a basic script

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ScribbleScrubber
 {
@@ -6,7 +7,26 @@ namespace ScribbleScrubber
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(Serialize(Process(File.ReadAllLines("path"))));
+        }
+        private static string[] Process(string[] input)
+        {
+            string[] output = new string[input.Length];
+            for(int i =0; i < input.Length; i++)
+                output[i] = input[i].Split('-')[1].Trim();
+            return output;
+        }
+        private static string Serialize(string[] input)
+        {
+            string output = "";
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (i != input.Length - 1)
+                    output += input[i] + ", ";
+                else
+                    output += input[i];
+            }
+            return output;
         }
     }
 }
